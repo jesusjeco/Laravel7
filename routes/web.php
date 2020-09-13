f50ad6ced553aa47f96e32df2008d1349c97c975
jesus carrero <jesusenrique.carrero@gmail.com>
2020-09-13 15:55:47 -0400

    menu1

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('person.index');
+    return view('welcome');
 });
+
+Route::resource('person', 'PersonController');
