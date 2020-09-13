029be8d15233a6d11050f435e9a7b2e5f46caaa9
jesus carrero <jesusenrique.carrero@gmail.com>
2020-09-13 12:57:00 -0400

    plantilla 1

---
+++ resources/views/person/index.blade.php
@@ -0,0 +1,7 @@
+@extends('templates.main-header')
+
+@section('name','Person index')
+
+@section('body')
+    Hola mundo
+@endsection
--- resources/views/templates/main-header.blade.php
+++ resources/views/templates/main-header.blade.php
@@ -4,9 +4,15 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+    <link rel="stylesheet" href="{{ asset('/css/app.css') }}">
+
     <title>@yield('name')</title>
 </head>
 <body>
 
+    @yield('body')
+
+    <script src="{{ asset('/js/app.js') }}"></script>
 </body>
 </html>
